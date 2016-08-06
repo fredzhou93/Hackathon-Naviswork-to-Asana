7b56e640d45d7693cf305f4f85ac480c30c891d0
fredzhou93 <fredzhou93@gmail.com>
2016-08-06 12:33:40 -0700

    Update script.js
    Add ajax calls

--- script.js
+++ script.js
@@ -27,3 +27,102 @@ $('#xml-upload').submit(function(e){
 		console.log("complete");
 	});
 });
+
+function execute() {
+	//Get Data from XML
+	let project;
+	let projectInfo = createProject(project);
+	
+	//Use the Data from projectInfo e.g. projectId and feed into the tasks
+	
+	//Check if it's update
+	let tasks;
+	tasks.forEach(function(task) {
+		createTask(task);
+	})
+}
+
+//Create Project
+function createProject(project) {
+	$.ajax({
+		url: 'https://app.asana.com/api/1.0/projects',
+		type: 'post',
+		data: project
+	})
+	.done(function(data) {
+		return data;
+	})
+	.fail(function() {
+		console.log("error");
+	});
+}
+
+//Create Individual Tasks
+function createTask(task) {
+	$.ajax({
+		url: 'https://app.asana.com/api/1.0/tasks',
+		type: 'post',
+		data: task
+	})
+	.done(function(data) {
+		return data;
+	})
+	.fail(function() {
+		console.log("error");
+	});
+}
+
+//Update Attachment
+function updateAttachment(file, id) {
+	let urlForAttachment = 'https://app.asana.com/api/1.0/tasks/' + id + '/attachments';
+	$.ajax({
+		url: urlForAttachment,
+		type: 'post',
+		data: file
+	})
+	.done(function(data) {
+		return data;
+	})
+	.fail(function() {
+		console.log("error");
+	});
+}
+
+//Get taskObject
+function getTaskObject(id) {
+	let urlForTasks = 'https://app.asana.com/api/1.0/projects/' + id + '/tasks';
+	$.ajax({
+		url: urlForTasks,
+		type: 'get',
+	})
+	.done(function(data) {
+		return data;
+	})
+	.fail(function() {
+		console.log("error");
+	});
+}
+
+
+//taskObject will be returned by GET https://app.asana.com/api/1.0/projects/project-id/tasks
+function turnTaskObjectToList(taskObject) {
+	let taskList = [];
+	taskObject = taskObject.data;
+	taskObject.forEach(function(element) {
+		taskList.push(element.id);
+	})
+	return taskList;
+}
+
+//Check whether a task already exist
+//oldTaskList will be returned by function turnTaskObjectToList
+function checkUpdateOrCreate(taskName, oldTaskList) {
+	if (oldTaskList.indexOf(taskName) === -1) {
+		return false;
+	}
+	else {
+		return true;
+	}
+	}
+}
+}
