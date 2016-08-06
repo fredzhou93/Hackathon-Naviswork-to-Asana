76a4a21b1cb2b65901302e9f6580ca9038f20538
w74 <wolfram.rong@gmail.com>
2016-08-06 13:31:18 -0500

    t1

--- upload_parser.php
+++ upload_parser.php
@@ -1,11 +1,31 @@
 <?php
 
+# Create a copy of the uploaded file
 $dir = 'uploads/';
-
 $filename = $dir . date('YmHis') . '.xml';
-
 move_uploaded_file($_FILES['file']['tmp_name'], $filename);
 
-$xml = simplexml_load_file($filename);
+
+# Begin data parse
+$arr = chunk($filename); // Chunk data
+
+
+
+
+
+function chunk($path_to_file){
+	$coll = new stdClass(); // Collection obj to return
+	$coll->tasks = []; // Tasks array of individual entries
+	
+	$xml = simplexml_load_file($filename);
+	$data_chunks = array(
+		'project_name' => $xml
+	);
+
+
+	return $data_chunks;
+}
+
+echo var_dump($arr);
 
 ?>
