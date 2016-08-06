86b8ccbf2b3082bfc08e948dad9525181ea04dd0
w74 <wolfram.rong@gmail.com>
2016-08-06 14:28:03 -0500

    xml parse 01

--- upload_parser.php
+++ upload_parser.php
@@ -9,23 +9,36 @@ move_uploaded_file($_FILES['file']['tmp_name'], $filename);
 # Begin data parse
 $arr = chunk($filename); // Chunk data
 
-
-
-
-
-function chunk($path_to_file){
-	$coll = new stdClass(); // Collection obj to return
-	$coll->tasks = []; // Tasks array of individual entries
-	
-	$xml = simplexml_load_file($filename);
+function chunk($fpath){
+	$xml = simplexml_load_file($fpath);
+	$clashtests = $xml->batchtest->clashtests->clashtest;
 	$data_chunks = array(
-		'project_name' => $xml
+		'project_name' => (string)$clashtests[0]->attributes()['name'],
+		'due_on' => date("Y-m-d", strtotime('+1 week')),
+		'tasks' => getTasks($clashtests)
 	);
-
 
 	return $data_chunks;
 }
 
-echo var_dump($arr);
+function getTasks($clashes){
+	$tasks = [];
+	foreach($clashes->clashresults->clashgroup as $clash){
+		$attr = $clash->attributes();
+		$temp = array(
+			'name' => (string)$attr['name'],
+			'gridlocation' => (string)$clash->gridlocation,
+			'photo' => (string)$attr['href'],
+			'distance' => (string)$attr['distance'],
+			'guid' => (string)$attr['guid']
+		);
+
+		array_push($tasks, $temp);
+	}
+
+	return $tasks;
+}
+
+echo json_encode($arr);
 
 ?>
